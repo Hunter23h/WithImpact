9aa7dd38e5f918f0ef958710e7adc203cf6d8a50
Kevin <yu.kevin2002@gmail.com>
2024-01-23 14:25:36 -0500

    fixed settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 40,
 }
 
 # Application definition
